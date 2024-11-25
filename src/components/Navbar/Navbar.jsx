7619d7237765d7b2a8933213f9171e86cb0a0f8d
mrauthentik <umokeuchenna2020@gmail.com>
2024-11-25 16:31:47 +0100

    Added buttons to the home page and styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -28,7 +28,9 @@ const Navbar = () => {
         <div className="navbar-profile">
           <img src={profile_img} alt=""  className='profile'/>
           <img src={caret_icon} alt="" />
-
+            <div className="dropdown">
+              <p>Sign Out of Netflix</p>
+            </div>
         </div>
       </div>
     </div>
