4cc0b8796c407dd3596d90d1eef390de5487faf2
mrauthentik <umokeuchenna2020@gmail.com>
2024-11-25 16:36:31 +0100

    pushing

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './TitleCards.css'
-
+import cards_data from '../../assets/cards/Cards_data'
 const TitleCards = () => {
   return (
     <div className='titlecards'>
