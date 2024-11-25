77e5ea9d5fadb75f54973a3b658494d609ed32f4
mrauthentik <umokeuchenna2020@gmail.com>
2024-11-25 16:41:59 +0100

    added title cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -4,7 +4,15 @@ import cards_data from '../../assets/cards/Cards_data'
 const TitleCards = () => {
   return (
     <div className='titlecards'>
-      
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {cards_data.map((card, index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p> {card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
