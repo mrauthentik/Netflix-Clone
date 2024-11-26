42bdc8d3efb0f40f8362e54ebf33af1d4d4ad3fa
mrauthentik <umokeuchenna2020@gmail.com>
2024-11-26 15:31:55 +0100

    created the user Sign Up function in firebase.js with firebase after installing firebase

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { addDoc, collection, getFirestore} from 'firebase/firestore'
+const firebaseConfig = {
+  apiKey: "AIzaSyBfO6qRHzgOAivbRD16Pxufrcan2hPa8k4",
+  authDomain: "netflix-clone-372aa.firebaseapp.com",
+  projectId: "netflix-clone-372aa",
+  storageBucket: "netflix-clone-372aa.firebasestorage.app",
+  messagingSenderId: "463848293281",
+  appId: "1:463848293281:web:e6fcc029bf4f84eff581e0"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+const db = getFirestore(app)
+//User SignUp function
+const signup = async (name, email, password)=>{
+     try {
+       const res = await createUserWithEmailAndPassword(auth, email, password)
+        const user= res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        })
+    } catch (error) {
+        console.log(error)
+        alert(error)
+     }
+}
+
+
