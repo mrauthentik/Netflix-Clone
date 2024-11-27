c02f8fa3515a2406a68723e2ac7ab170b0829531
mrauthentik <umokeuchenna2020@gmail.com>
2024-11-27 17:24:59 +0100

    Finish touches, Last Push to repo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,7 +20,9 @@ const Home = () => {
             <button className='btn'> <img src={play_icon} alt="" />Play</button>
             <button className='btn dark-btn'> <img src={info_icon} alt="" />More Info</button>
            </div>
-           <TitleCards />
+            <div className="title-cards">
+           <TitleCards  />
+            </div>
         </div>
       </div>
       <div className="more-cards">
